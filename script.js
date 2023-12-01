3757dde0ed0b8ef997101d1296cec41e1dcedd63
sungyejii <sungyejoy@gmail.com>
2023-12-01 21:32:20 +0800

    Commented and labelled the codes

---
+++ script.js
@@ -0,0 +1,10 @@
+// Scroll to top function in Javascript //
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        left: 0, // Allows to scroll to the specified coordinates (top: 0, left: 0)//
+        behavior: 'smooth' // Allows the scrolling to top to be smooth //
+    });
+}
+
+
